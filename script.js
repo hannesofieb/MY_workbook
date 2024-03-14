7499d03d46ef4f9d8d22119c46975b8c1a4f7a9d
Hanne Sofie <bergh@student.unimelb.edu.au>
2024-03-15 01:02:02 +1100

    added more material (img to html), made image gallery scrollable, failed to add cursor, changed background and font colour

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-<script>"use strict";
-
-        document.addEventListener("click", event => {
-            const img = event.target.closest(".toggle-image");
-            if (img && img.nextElementSibling.tagName === "FIGCAPTION") {
-                img.nextElementSibling.classList.toggle("hidden");
-            }
-        });
-</script>
+function captionAppear(){
+    document.addEventListener("click", event => {
+        const img = event.target.closest(".toggle-image");
+        if (img && img.nextElementSibling.tagName === "FIGCAPTION") {
+            img.nextElementSibling.classList.toggle("hidden");
+        }
+    });
+}
 
 // What you could do is create a function which resizes your image and caption, by applying new class 
 // names that have styling to make tgh eimage full page width
+
+
+function pageScroll() {
+    window.scrollBy(1,0);
+    scrolldelay = setTimeout(pageScroll,10);
+}
