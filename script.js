9174742020a26124adc7e8a3cfca8064656820bd
Hanne Sofie <bergh@student.unimelb.edu.au>
2024-03-15 12:55:19 +1100

    stye fixes

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ function pageScroll() {
     window.scrollBy(1,0);
     scrolldelay = setTimeout(pageScroll,10);
 }
+
+function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    var scrollSpeed = 60; // Janky jank <<<<<<<<<<<<<<
+    document.documentElement.scrollLeft -= (delta * scrollSpeed);
+    document.body.scrollLeft -= (delta * scrollSpeed);
+    e.preventDefault();
+  }
+  
